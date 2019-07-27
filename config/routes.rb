94b43ace000d35635507bde719b0c7a679229581
Freddyvl <freddy.vl@me.com>
2019-07-27 22:05:20 +0200

    added pay and cart

--- config/routes.rb
+++ config/routes.rb
@@ -9,6 +9,14 @@ Rails.application.routes.draw do
 
         resources :current_orders
 
+        member do
+
+          get "pay"
+
+          get "cart"
+
+        end
+
         end
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
